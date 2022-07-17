1fd4849741ec1ac85d40e89a06b118396c12a803
FilipeSilva <flpsilvatj@gmail.com>
2022-07-17 08:13:13 -0300

    Original com 2 paginas prontas.

---
+++ _js/javascript.js
@@ -0,0 +1,42 @@
+let darkModeBtn = document.querySelector('.dm-button');
+let AllPage = document.querySelector('.page-mode');
+        
+// MENU E MENU MOBILE
+function onMenuShow(){
+    let menuMobile = document.querySelector('.mobile-menu');
+    if (menuMobile.classList.contains('open')) {
+        menuMobile.classList.remove('open');
+    } else {
+        menuMobile.classList.add('open');
+    }
+}
+// DARK MODE
+function DarkMode(){
+    if (darkModeBtn.classList.contains('active')) {
+        darkModeBtn.classList.remove('active');
+        AllPage.classList.remove('dark-mode');
+
+        localStorage.setItem('DarkMode', 0); /* 01 */
+    } else {
+        darkModeBtn.classList.add('active');
+        AllPage.classList.add('dark-mode');
+
+        localStorage.setItem('DarkMode', 1); /* 01 */
+    }
+}
+// VERIFICAR SE A PÁGINA ESTÁ EM DARK MODE
+function onDarkModeChecked(){
+    if (localStorage.getItem('DarkMode') == 1) {
+        darkModeBtn.classList.add('active');
+        AllPage.classList.add('dark-mode');
+    }
+}
+
+/* 
+
+(01)    =   A sessionStorage e localStorage são recursos do HTML5 que permite que você salve na sessão do navegador, de forma fixa (localStorage) ou temporária (sessionStorage) algumas informações, e com isso você pode acessar na outra página.
+
+    localStorage.setItem('DarkMode', 0);    -   Adiciona uma Storage
+    localStorage.getItem('DarkMode');       -   Recupera a Storage
+
+*/
